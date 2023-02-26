9d8a45d4a6e98a497908abfb9f3fb0986149fdd3
eva-gor <evagor82@gmail.com>
2023-02-26 13:08:16 +0200

    HW # 34

--- src/telran/structure/MultiCountersStructure.java
+++ src/telran/structure/MultiCountersStructure.java
@@ -7,19 +7,13 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class MultiCountersStructure implements MultiCounters {
-/*
- * structObjects - HashMap that stores objects as a key and frequency of their occurrence as a value
- * structFrequency - HashMap that stores occurrence as a key and objects as a value
- * listFrequency - stores ordered sequence of frequencies that exists in HashMaps
- */
+
 	HashMap<Object, Integer> structObjects;
-	HashMap<Integer, HashSet<Object>> structFrequency;
-	ArrayList<Integer> listFrequency;
+	ArrayList<HashSet<Object>> listFrequency;
 
 	
 	public MultiCountersStructure() {
 		structObjects = new HashMap<> ();
-		structFrequency = new HashMap<>();
 		listFrequency = new ArrayList<>();
 	}
 	
@@ -30,49 +24,21 @@ public class MultiCountersStructure implements MultiCounters {
 		if (counter == null) {
 			counter = 0;
 		}
-		structObjects.remove(item);
-		counter++;
-		structObjects.put(item, counter);
+		structObjects.put(item, ++counter);
 				
-		// add, if not exists, frequency to the listFrequency
-		int insertionPoint = getInsertionPoint(counter);
-
-		// looking for objects with the same frequency and delete previous frequency
-		addWithNewFrequency(item, counter);
-		deleteFromFrequencies(item, counter - 1, insertionPoint);		
-		
+		if (counter > listFrequency.size()) {
+			listFrequency.add(new HashSet<>());
+		}
+		resettleObjectInList(item, counter, counter - 1);
 		return counter;
 	}
 
-	private int getInsertionPoint(Integer counter) {
-		int insertionPoint = Collections.binarySearch(listFrequency, counter);
-		if (insertionPoint < 0) {
-			insertionPoint = -1-insertionPoint;
-			listFrequency.add(insertionPoint, counter);
-		}
-		return insertionPoint;
-	}
-
-	private void deleteFromFrequencies(Object item, Integer counter, int insertionPoint) {
-		HashSet<Object> set = structFrequency.get(counter);
-		if (set != null) {
-			set.remove(item);
-			if (set.size() == 0) {
-				structFrequency.remove(counter);
-				listFrequency.remove(insertionPoint-1);
-			}
-		}
-		
-	}
-
-	private void addWithNewFrequency(Object item, Integer counter) {
-		HashSet<Object> set = structFrequency.get(counter);
-		if (set == null) {
-			HashSet<Object> newSet = new HashSet<Object>();
-			newSet.add(item);
-			structFrequency.put(counter, newSet);
-		} else {
-			set.add(item);
+	private void resettleObjectInList(Object item, Integer counterNew, Integer counterOld) {
+		HashSet<Object> setNew = listFrequency.get(counterNew - 1); // counter - 1 e.g. indexing starts from 0
+		setNew.add(item);
+		if (counterOld > 0) {
+			HashSet<Object> setOld = listFrequency.get(counterOld - 1);
+			setOld.remove(item); 	// remove the object with old frequency
 		}
 	}
 	
@@ -84,17 +50,23 @@ public class MultiCountersStructure implements MultiCounters {
 	@Override
 	public boolean remove(Object item) {
 		Integer counter = getValue(item);
-		structObjects.remove(item);
 		if (counter != null) {
-			Integer position = getInsertionPoint(counter);
-			deleteFromFrequencies(item, counter, position);			
+			structObjects.remove(item);
+			HashSet<Object> set = listFrequency.get(counter - 1);
+			set.remove(item);
+			if (counter > 0) {
+				listFrequency.get(counter - 2).add(item);
+			}
+			if (counter == listFrequency.size() && set.isEmpty()) {
+				listFrequency.remove(counter - 1);
+			}
 		}
 		return counter == null ? false : true;
 	}
 
 	@Override
 	public Set<Object> getMaxItems() {
-		return structFrequency.get(listFrequency.get(listFrequency.size()-1));
+		return listFrequency.get(listFrequency.size()-1);
 	}
 
 }
